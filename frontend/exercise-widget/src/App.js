b57aecc22511d736fe959b6aca0508616c309dca
David Kuehnemann <david.kuehnemann@ru.nl>
2023-02-13 16:25:32 +0100

    Populate drop down menu with options

--- frontend/exercise-widget/src/App.js
+++ frontend/exercise-widget/src/App.js
@@ -4,11 +4,25 @@ import Annotatable from './Annotatable'
 
 class App extends Component {
   render() {
+    const startOptions = [
+      ['%L', '!%L'],
+      ['%H', '!%H'],
+      ['%HL', '!%HL']]
+    const wordOptions = [
+      ['H*', '!H*'],
+      ['H*L', '!H*L'],
+      ['L*', 'L*H'],
+      ['L*HL', 'L*!HL'],
+      ['H*LH', '∅']
+    ]
+    const endOptions = [['L%'], ['H%'], ['%']]
     return (
       <div className="App">
         <Annotatable preset="%L"/>
-        <Annotatable text="Dat"/> weet ik <Annotatable text="ook"/> niet <Annotatable text="zei de"/> traploper
-        <Annotatable />
+        <Annotatable options={wordOptions} text="Dat"/> weet ik <Annotatable options={wordOptions} text="ook"/> niet
+        <Annotatable options={wordOptions}/>
+        <Annotatable options={wordOptions} text="zei de"/> traploper
+        <Annotatable options={endOptions}/>
       </div>
     );
   }
