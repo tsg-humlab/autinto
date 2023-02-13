bda776a71b6d22fdc49b621fb386a55f7e7f63fd
David Kuehnemann <david.kuehnemann@ru.nl>
2023-02-13 15:37:52 +0100

    Implement drop down menu

--- frontend/exercise-widget/src/Annotatable.js
+++ frontend/exercise-widget/src/Annotatable.js
@@ -1,11 +1,28 @@
 import React, { Component } from 'react';
 
+function Dropdown() {
+  return (
+    <div className="dropdown-offset">
+      <div className="dropdown">
+        <table>
+          <tr>
+            <td>H</td>
+            <td>L</td>
+          </tr>
+        </table>
+      </div>
+    </div>
+  )
+}
+
 export default function Annotatable({ text="", preset=null }) {
   return (
     <div className={`annotatable ${text ? '' : 'empty'} ${preset ? 'disabled' : ''}`}>
       {text ? text : <React.Fragment> &nbsp; </React.Fragment>}
       <div className="box">
         <div className="box-content">{preset}</div>
+        {preset ? // Only allow for a dropdown if the value is editable (no preset).
+          '' : <Dropdown />}
       </div>
     </div>
   )
