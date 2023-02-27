8cc6d85e9ccadeb517b7eee0f8b07f6c5af42c10
David Kuehnemann <david.kuehnemann@ru.nl>
2023-02-27 12:07:07 +0100

    Run prettier on all existing source code

--- frontend/exercise-widget/src/Annotatable.js
+++ frontend/exercise-widget/src/Annotatable.js
@@ -1,38 +1,55 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 
 function Dropdown({ open, onSelected, options }) {
   return (
     <div className="dropdown-offset">
-      <div className={`dropdown ${open ? 'dropdown-active' : ''}`}>
+      <div className={`dropdown ${open ? "dropdown-active" : ""}`}>
         <table>
           <tbody>
-          {options.map((rowOptions, index) => 
-          <tr key={index}>
-            {rowOptions.map((option) =>
-            <td key={option} onClick={() => onSelected(option)}>{option}</td>
-            )}
-          </tr>
-          )}
+            {options.map((rowOptions, index) => (
+              <tr key={index}>
+                {rowOptions.map((option) => (
+                  <td key={option} onClick={() => onSelected(option)}>
+                    {option}
+                  </td>
+                ))}
+              </tr>
+            ))}
           </tbody>
         </table>
       </div>
     </div>
-  )
+  );
 }
 
-export default function Annotatable({ text="", preset=null, options=[] }) {
-  const [menuOpened, setMenuOpened] = useState(false)
-  const [selectedValue, selectValue] = useState(preset)
+export default function Annotatable({
+  text = "",
+  preset = null,
+  options = [],
+}) {
+  const [menuOpened, setMenuOpened] = useState(false);
+  const [selectedValue, selectValue] = useState(preset);
 
   return (
-    <div onClick={() => setMenuOpened(open => !open)}
-         className={`annotatable ${text ? '' : 'empty'} ${preset ? 'disabled' : ''}`}>
+    <div
+      onClick={() => setMenuOpened((open) => !open)}
+      className={`annotatable ${text ? "" : "empty"} ${
+        preset ? "disabled" : ""
+      }`}
+    >
       {text ? text : <React.Fragment> &nbsp; </React.Fragment>}
       <div className="box">
         <div className="box-content">{selectedValue}</div>
-        {preset ? // Only allow for a dropdown if the value is editable (no preset).
-            '' : <Dropdown options={options} open={menuOpened} onSelected={selectValue} />}
+        {preset ? ( // Only allow for a dropdown if the value is editable (no preset).
+          ""
+        ) : (
+          <Dropdown
+            options={options}
+            open={menuOpened}
+            onSelected={selectValue}
+          />
+        )}
       </div>
     </div>
-  )
+  );
 }
--- frontend/exercise-widget/src/App.js
+++ frontend/exercise-widget/src/App.js
@@ -1,28 +1,30 @@
-import React, { Component } from 'react';
-import './App.css';
-import Annotatable from './Annotatable'
+import React, { Component } from "react";
+import "./App.css";
+import Annotatable from "./Annotatable";
 
 class App extends Component {
   render() {
     const startOptions = [
-      ['%L', '!%L'],
-      ['%H', '!%H'],
-      ['%HL', '!%HL']]
+      ["%L", "!%L"],
+      ["%H", "!%H"],
+      ["%HL", "!%HL"],
+    ];
     const wordOptions = [
-      ['H*', '!H*'],
-      ['H*L', '!H*L'],
-      ['L*', 'L*H'],
-      ['L*HL', 'L*!HL'],
-      ['H*LH', '∅']
-    ]
-    const endOptions = [['L%'], ['H%'], ['%']]
+      ["H*", "!H*"],
+      ["H*L", "!H*L"],
+      ["L*", "L*H"],
+      ["L*HL", "L*!HL"],
+      ["H*LH", "∅"],
+    ];
+    const endOptions = [["L%"], ["H%"], ["%"]];
     return (
       <div className="App">
-        <Annotatable preset="%L"/>
-        <Annotatable options={wordOptions} text="Dat"/> weet ik <Annotatable options={wordOptions} text="ook"/> niet
-        <Annotatable options={wordOptions}/>
-        <Annotatable options={wordOptions} text="zei de"/> traploper
-        <Annotatable options={endOptions}/>
+        <Annotatable preset="%L" />
+        <Annotatable options={wordOptions} text="Dat" /> weet ik{" "}
+        <Annotatable options={wordOptions} text="ook" /> niet
+        <Annotatable options={wordOptions} />
+        <Annotatable options={wordOptions} text="zei de" /> traploper
+        <Annotatable options={endOptions} />
       </div>
     );
   }
