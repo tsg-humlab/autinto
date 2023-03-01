2a96b70c0a2c997689613a9de9a25c43725ac6f5
David Kuehnemann <david.kuehnemann@ru.nl>
2023-03-01 12:32:54 +0100

    Hook widget onto each div with .exercise class

--- frontend/exercise-widget/src/index.js
+++ frontend/exercise-widget/src/index.js
@@ -1,6 +1,9 @@
 import React from "react";
-import ReactDOM from "react-dom";
+import { createRoot } from "react-dom/client";
 import App from "./App";
 import "./index.css";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+document.querySelectorAll('.exercise').forEach(elem => {
+  const root = createRoot(elem)
+  root.render(<App />)
+})
