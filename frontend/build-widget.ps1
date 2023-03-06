6724aa121f8879b69c144fba3a3fb4151573fc4f
Baraa-k <82050886+Baraa-k@users.noreply.github.com>
2023-03-06 17:02:40 +0100

    Windows build script

---
+++ frontend/build-widget.ps1
@@ -0,0 +1,5 @@
+cd exercise-widget
+npm run build
+cd ..
+cp .\exercise-widget\build\static\css\main.*.css .\book\additional-files\main.css
+cp .\exercise-widget\build\static\js\main.*.js .\book\additional-files\main.js
