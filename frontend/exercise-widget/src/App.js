dd66e6924fd8b8c362520a48b11351ab3845332e
David Kuehnemann <david.kuehnemann@ru.nl>
2023-03-09 16:00:39 +0100

    Add play button

--- frontend/exercise-widget/src/App.js
+++ frontend/exercise-widget/src/App.js
@@ -2,23 +2,33 @@ import React, { Component } from "react";
 import "./App.css";
 import Annotatable from "./Annotatable";
 
-class App extends Component {
-  render() {
-    const startOptions = [
-      ["%L", "!%L"],
-      ["%H", "!%H"],
-      ["%HL", "!%HL"],
-    ];
-    const wordOptions = [
-      ["H*", "!H*"],
-      ["H*L", "!H*L"],
-      ["L*", "L*H"],
-      ["L*HL", "L*!HL"],
-      ["H*LH", "∅"],
-    ];
-    const endOptions = [["L%"], ["H%"], ["%"]];
-    return (
-      <div className="App">
+import "bootstrap/dist/css/bootstrap.min.css"
+import "bootstrap/dist/js/bootstrap.bundle.min"
+
+function App () {
+  const startOptions = [
+    ["%L", "!%L"],
+    ["%H", "!%H"],
+    ["%HL", "!%HL"],
+  ];
+  const wordOptions = [
+    ["H*", "!H*"],
+    ["H*L", "!H*L"],
+    ["L*", "L*H"],
+    ["L*HL", "L*!HL"],
+    ["H*LH", "∅"],
+  ];
+  const endOptions = [["L%"], ["H%"], ["%"]];
+
+
+  function playAudio() {
+    const audio = new Audio('audio/109.mp3')
+    audio.play()
+  }
+
+  return (
+    <div className="App">
+      <div className="text">
         <Annotatable preset="%L" />
         <Annotatable options={wordOptions} text="Dat" /> weet ik{" "}
         <Annotatable options={wordOptions} text="ook" /> niet
@@ -29,8 +39,11 @@ class App extends Component {
         <Annotatable options={endOptions} />
         </span>
       </div>
-    );
-  }
+      <div className="button-container ml-3">
+        <button className="btn btn-primary mt-3 pl-1" onClick={playAudio}>Play</button>
+      </div>
+    </div>
+  );
 }
 
 export default App;
