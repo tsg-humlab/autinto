45478dc69009caa974f518e9da8a86bd78d22210
Athena Luka <tlreijers@posteo.net>
2023-05-19 17:19:59 +0200

    wrapper around resynthesis functions

--- resynthesis/resynthesis.py
+++ resynthesis/resynthesis.py
@@ -1,3 +1,20 @@
+import subprocess
+
 from . import sentence_parsing
 from . import praatcall
 
+def resynthesize(sentence, textgrid_file, audio_file, optional_args=None):
+    new_textgrid = sentence_parsing.run(textgrid_file, sentence)
+    (new_audio, pdf_image) = praatcall.send_to_praat(audio_file, new_textgrid)
+    svg_image = pdf_to_svg(pdf_image)
+
+    return (new_audio, svg_image)
+
+
+def send_to_praat(audio_file, textgrid)
+    pass
+
+
+def pdf_to_svg(pdf_bytes):
+    completed_process = subprocess.run(['pdftocairo', '-svg', '-', '-'], input=pdf_bytes, stdout=subprocess.PIPE)
+    return completed_process.stdout
