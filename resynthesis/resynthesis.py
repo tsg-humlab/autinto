f38976a9e8419b146ed6e89278de29e9bec8cac6
Athena Luka <tlreijers@posteo.net>
2023-05-25 18:05:29 +0200

    cleaned up interface

--- resynthesis/resynthesis.py
+++
@@ -1,7 +0,0 @@
-from resynthesis.phrase import Phrase
-from resynthesis.resynthesized import ResynthesizedPhrase
-
-def resynthesize(sentence, textgrid, audio, **kwargs):
-    phrase = Phrase(textgrid)
-    resynth_phrase = ResynthesizedPhrase(phrase, sentence)
-    resynth_points = resynth_phrase.decode()
