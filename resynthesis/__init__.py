16e7ff37b1a58b71aae00e9bf38f4872d2b50d9c
Athena Luka <tlreijers@posteo.net>
2023-06-02 01:27:07 +0200

    framework updates

--- resynthesis/__init__.py
+++ resynthesis/__init__.py
@@ -1,7 +1,19 @@
 from resynthesis.phrase import Phrase
 from resynthesis.resynthesized import ResynthesizedPhrase
 
-def resynthesize(sentence, textgrid, audio, **kwargs):
-    phrase = Phrase(textgrid)
+import textgrid as tg
+
+def resynthesize(sentence, textgrid_filename, audio_filename, **kwargs):
+    phrase = Phrase(textgrid_filename)
     resynth_phrase = ResynthesizedPhrase(phrase, sentence)
     resynth_points = resynth_phrase.decode()
+
+    resynthesized_textgrid = update_textgrid(textgrid_filename, resynth_points)
+
+
+def send_to_praat(audio_filename, resynthesized_textgrid):
+    pass
+
+
+def update_textgrid(textgrid_filename, resynth_points) -> tg.TextGrid:
+    grid = tg.TextGrid.fromFile(textgrid_filename)
