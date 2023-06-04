30edb833a4dfb8a47b1764ddcb79029a1fffd36a
Timon <Timon L. Reijers>
2023-06-04 17:41:34 +0200

    Changed time to explicit types, added checks to make sure this is handled correctly

--- resynthesis/phrase.py
+++ resynthesis/phrase.py
@@ -1,11 +1,13 @@
 from dataclasses import dataclass
 import textgrid as tg
 
-from resynthesis.types import Milliseconds, Interval
+from resynthesis.types import Seconds, Interval
 
 @dataclass
 class VoicedPortion(Interval):
-    def __init__(self, start_time, end_time):
+    def __init__(self, textgrid_vp):
+        start_time = Seconds(textgrid_vp.minTime)
+        end_time = Seconds(textgrid_vp.maxTime)
         super().__init__(start_time, end_time)
 
 
@@ -13,19 +15,26 @@ class VoicedPortion(Interval):
 class IntonationalPhrase(Interval):
     vps: list[VoicedPortion]
 
-    def __init__(self, start_time, end_time, vps):
+    def __init__(self, textgrid_ip, vps):
+        start_time = Seconds(textgrid_ip.minTime)
+        end_time = Seconds(textgrid_ip.maxTime)
+
         super().__init__(start_time, end_time)
         self.vps = vps
 
 
 @dataclass
 class Phrase(Interval):
-    textgrid: tg.TextGrid
     ips: list[IntonationalPhrase]
+    textgrid: tg.TextGrid
 
     def __init__(self, file: str):
         self.textgrid = tg.TextGrid.fromFile(file) 
-        super().__init__(self.textgrid.minTime, self.textgrid.maxTime)
+
+        start_time = Seconds(self.textgrid.minTime)
+        end_time = Seconds(self.textgrid.maxTime)
+
+        super().__init__(start_time, end_time)
 
         textgrid_ips = self.textgrid.getFirst("IP's")
         textgrid_vps = self.textgrid.getFirst('vp')
@@ -34,14 +43,14 @@ class Phrase(Interval):
         for textgrid_ip in textgrid_ips:
             if not textgrid_ip.mark:
                 continue
-            ip = IntonationalPhrase(textgrid_ip.minTime, textgrid_ip.maxTime, [])
+            ip = IntonationalPhrase(textgrid_ip, [])
             self.ips.append(ip)
 
         for textgrid_vp in textgrid_vps:
             if not textgrid_vp.mark:
                 continue
 
-            vp = VoicedPortion(textgrid_vp.minTime, textgrid_vp.maxTime)
+            vp = VoicedPortion(textgrid_vp)
             # Put this VP in the IP that its start_time falls in.
             done = False
             for ip in self.ips:
--- resynthesis/types.py
+++ resynthesis/types.py
@@ -1,18 +1,37 @@
+from datetime import timedelta
 from dataclasses import dataclass
 
-Milliseconds = int
+def Seconds(seconds: float):
+    return timedelta(seconds=seconds)
+
+def Milliseconds(milliseconds: int):
+    # used to be a type
+    return timedelta(milliseconds=milliseconds)
+
 Frequency = int # in Hz
 FrequencyDiff = Frequency
 Scalar = float
 
 @dataclass
 class Interval:
-    start_time: Milliseconds
-    end_time:   Milliseconds
+    start_time: timedelta
+    end_time:   timedelta
 
-    def __init__(self, start_time: float, end_time: float):
-        self.start_time = Milliseconds(start_time*1000)
-        self.end_time = Milliseconds(end_time*1000)
+    def __init__(self, start_time, end_time):
+        # Make sure that start_time and end_time are actually timedeltas,
+        # and not ints or floats without unit.
+        assert (isinstance(start_time, timedelta)
+                and isinstance(end_time, timedelta)), \
+               'Error instantiating an {}: Expected {}, found {} and {}'.format(
+                   type(self).__name__,
+                   timedelta,
+                   type(start_time),
+                   type(end_time)
+                )
+
+        self.start_time = start_time
+        self.end_time = end_time
+
 
 
 @dataclass
@@ -32,13 +51,13 @@ class FrequencyRange:
 class FrequencyPoint:
     label: str
     freq: Frequency
-    time: Milliseconds
+    time: timedelta
 
 
 @dataclass
 class ResynthesizeVariables:
-    to_time: Milliseconds = Milliseconds(90)
-    from_time: Milliseconds = Milliseconds(100)
+    to_time: timedelta = Milliseconds(90)
+    from_time: timedelta = Milliseconds(100)
     star_time: Scalar = 0.3
 
     phrasal_downstep: Scalar = 0.9
