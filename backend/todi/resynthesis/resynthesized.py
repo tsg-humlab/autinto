7156a101ae9e33731eeaaca5222b0a8d4a259fc2
Sebastiaan-Grein <sebastiaan@grein.nl>
2023-06-05 14:05:09 +0200

    documentation added

--- backend/todi/resynthesis/resynthesized.py
+++ backend/todi/resynthesis/resynthesized.py
@@ -23,16 +23,21 @@ class ResynthesizedIntonationalPhrase:
     _frequency_range: FrequencyRange
 
     def __init__(self, phrase_ip: IntonationalPhrase, sentence: deque[str], parent: ResynthesizedPhrase):
+        """ In this function, we create a ResynthesisedIntonationalPhrase,
+         It consists of an initial boundary, all the words with a pitchaccent, and a final boundary """
+        
         self.ip = phrase_ip
         self.parent = parent
         
+        #Set the initial boundary and do extra processing for unaccented ip's
         str_initial_boundary = sentence.popleft()
         self.checkUnaccented(str_initial_boundary, sentence)
         self.initial_boundary = InitialBoundary(str_initial_boundary, self)
+        
 
         
 
-
+        #add all words with pitch accent to a list
         self.words: list[Word] = []
         for voiced_portion in phrase_ip.vps:
             str_word = sentence.popleft()
@@ -42,7 +47,8 @@ class ResynthesizedIntonationalPhrase:
                             len(self.words),
                             voiced_portion)
                 self.words.append(word)
-
+        
+        #Set the final boundary.
         str_final_boundary = sentence.popleft()
         self.final_boundary = FinalBoundary(str_final_boundary, self)
 
@@ -53,6 +59,8 @@ class ResynthesizedIntonationalPhrase:
         # Reset frequency_range
         self.reset_downstep()
 
+        #decode all pitch accents in the ResynthesisedIntonationalPhrase. 
+        #decode explained in pitch_accents.py for each class initial_boundary, word and final_boundary
         self.initial_boundary.decode(point_list)
         for word in self.words:
             word.decode(point_list)
@@ -60,6 +68,7 @@ class ResynthesizedIntonationalPhrase:
 
         self.parent._frequency_range = self.frequency_range
 
+    #these @property defenitions are used to more easily retrieve properties from a previous type within this class.
     @property
     def vars(self) -> ResynthesizedVariables:
         return self.parent.vars
@@ -78,14 +87,18 @@ class ResynthesizedIntonationalPhrase:
         else:
             return self.parent.frequency_range
 
+    #apply the frequency scaling for a downstep.
     def downstep(self, scalar):
         freq_low = self.vars.fr + scalar*(self.frequency_range.low - self.vars.fr)
         freq_high = self.vars.fr + scalar*(self.frequency_range.high - self.vars.fr)
         self._frequency_range = FrequencyRange(freq_low, freq_high)
 
+    #reset downstep
     def reset_downstep(self):
         self._frequency_range = None
     
+    #Check if there is a unaccented ip, if this is the case, we set it as an initial boundary and add an empty word
+    #to keep our vp indexing consistent.
     def checkUnaccented(self, str_initial_boundary, sentence):
         if str_initial_boundary in {"H", "L"}:
             sentence.insert(0, None)
@@ -98,9 +111,14 @@ class ResynthesizedPhrase:
     vars: ResynthesizeVariables
 
     def __init__(self, phrase: Phrase, sentence: list[str], **kwargs):
+        """We recieve a list of Those parameters that the student has changed, Here we check
+        which values have been declared and which have not, those left empty are updated
+        with the default values"""
+
         self.ips: list[ResynthesizedIntonationalPhrase] = []
         self.textgrid = phrase.textgrid
 
+        #check the gender and set default values accordingly
         gender = self.textgrid.getFirst('words')[0].mark
         match gender:
             case 'm':
@@ -118,6 +136,7 @@ class ResynthesizedPhrase:
                 if 'w' not in kwargs:
                     kwargs['w'] = 190
 
+        #set remaining default values if neccessary.
         self.vars = ResynthesizeVariables(**kwargs)
 
         sentence = deque(sentence)
@@ -125,18 +144,29 @@ class ResynthesizedPhrase:
             ip = ResynthesizedIntonationalPhrase(phrase_ip, sentence, self)
             self.ips.append(ip)
 
+        #Set the initial frequencies
         freq_low = self.vars.fr + self.vars.n - 0.5*self.vars.w
         freq_high = self.vars.fr + self.vars.n + 0.5*self.vars.w
         self._frequency_range = FrequencyRange(freq_low, freq_high)
 
+
+    
     def decode(self):
+        """
+        decode all ResynthesizedIntonationalPhrase, by looping through each of them and calling their own decode function on it.
+        """
         point_list = []
         for ip in self.ips:
+
+            #Decode each ResynthesizedIntonationalPhrase
             ip.decode(point_list)
         return point_list
 
 
     def decode_into_textgrid(self):
+        """
+        Add the word_tier, target_tier and frequency_tiers to the textgrid with their correct corresponding targetlabels, timings and frequency.
+        """
         textgrid = copy.deepcopy(self.textgrid)
 
         # Add word labels
@@ -151,12 +181,16 @@ class ResynthesizedPhrase:
         # Generate the new frequency points
         point_list = self.decode()
 
+        #create tier objects.
         target_tier = tg.PointTier('targets', self.textgrid.minTime, self.textgrid.maxTime)
         frequency_tier = tg.PointTier('ToDI-F0', self.textgrid.minTime, self.textgrid.maxTime)
 
+        #add the newly generated frequency_points to the target_tier and frequency_tier
         for frequency_point in point_list:
             target_tier.addPoint(tg.Point(frequency_point.time.total_seconds(), frequency_point.label))
             frequency_tier.addPoint(tg.Point(frequency_point.time.total_seconds(), str(int(frequency_point.freq))))
+        
+        #add tier to textgrid
         textgrid.append(target_tier)
         textgrid.append(frequency_tier)
 
@@ -167,6 +201,7 @@ class ResynthesizedPhrase:
     def frequency_range(self):
         return self._frequency_range
 
+    #Set the frequency values to the downstepped values.
     def downstep(self, scalar):
         freq_low = self.vars.fr + scalar*(self.frequency_range.low - self.vars.fr)
         freq_high = self.vars.fr + scalar*(self.frequency_range.high - self.vars.fr)
