9fb1eb9eefda8504fed5ca5242bd6d7635b68558
Micha Hulsbosch <michahulsbosch@gmail.com>
2026-08-31 08:29:00 +0000

    Add LOGGING setting

--- todi/settings.py
+++ todi/settings.py
@@ -85,6 +85,28 @@ DATABASES = {
 }
 
 
+LOGGING = {
+    "version": 1,
+    "disable_existing_loggers": False,
+    "handlers": {
+        "console": {
+            "class": "logging.StreamHandler",
+        },
+    },
+    "root": {
+        "handlers": ["console"],
+        "level": "WARNING",
+    },
+    "loggers": {
+        "django": {
+            "handlers": ["console"],
+            "level": os.getenv("DJANGO_LOG_LEVEL", "INFO"),
+            "propagate": False,
+        },
+    },
+}
+
+
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
 
